c4c245289cc2ce478d62a10d5bef22c552d486b2
Ovayo <152535186+Ovayo-Ntebe@users.noreply.github.com>
2026-08-09 01:58:58 +0200

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        #Note to self! Sets automatically remove dupicates so to effectivly check if there is a duplicate then compare the sizes
+        # Solution is O(N)
+        numSet = set(nums)
+    
+        if len(numSet) < len(nums):
+            return True
+        else:
+            return False
+
+ # potentially sets as they dont want duplicates(#if myset properly exists then it means no duplicatates so it should have defaulted to if its corerect then its false but it only works for one test case)
+ #       mySet = set(nums)
+ #       if mySet:
+ #           return False 
+ #       else:
+ #           return True 
+
+ 
+    
