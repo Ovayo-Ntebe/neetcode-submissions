fe019fa8dfd980f22e29fa06524803e07600bd27
Ovayo <152535186+Ovayo-Ntebe@users.noreply.github.com>
2026-08-09 13:45:01 +0200

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #I will attempt a brute force of this first
+        #Loop through each index and compare if its sum is equal to 
+        #target and so on so on
+        indices = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    indices.append(i)
+                    indices.append(j)
+        return indices
+
+                
+
+        
